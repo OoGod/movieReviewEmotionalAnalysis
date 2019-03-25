94e7cb0fb88bcaa3968fa08d7d4619f15e805c64
OoGod <1617605689@qq.com>
2019-03-25 15:39:57 +0800

    crawl reviews

---
+++ parse_xiao1.py
@@ -0,0 +1,46 @@
+# coding=utf-8
+from selenium import webdriver  
+from selenium.webdriver.common.keys import Keys  
+import selenium.webdriver.support.ui as ui  
+from selenium.webdriver.common.action_chains import ActionChains  
+import time      
+import re      
+import os
+import csv
+
+#打开Firefox浏览器
+driver = webdriver.PhantomJS()
+i = 0
+while i<1:
+    num = i*20
+    url = "https://movie.douban.com/subject/1292052/comments?start=" + str(num) +"&limit=20&sort=new_score&status=P"
+    print(url)
+    driver.get(url)
+    #用户姓名 超链接
+    elem1 = driver.find_elements_by_xpath("//div[@class='avatar']/a")
+    for n in elem1:
+        print(n.get_attribute("title")), 
+        print(n.get_attribute("href"))
+        
+    #用户评分
+    elem2 = driver.find_elements_by_xpath("//span[@class='comment-info']/span[2]")
+    for n in elem2:
+        print(n.get_attribute("class")), 
+        print(n.get_attribute("title"))
+
+    #有用数
+    elem3 = driver.find_elements_by_xpath("//span[@class='comment-vote']/span[1]")
+    for n in elem3:
+        print(n.text)
+        
+    #日期
+    elem4 = driver.find_elements_by_xpath("//span[@class='comment-time ']")
+    for n in elem4:
+        print(n.text)
+
+    #评论
+    elem5 = driver.find_elements_by_xpath("//span[@class='short']")
+    for n in elem5:
+        print(n.text)
+        
+    i = i + 1
---
+++ parse_xiao2.py
@@ -0,0 +1,33 @@
+# coding=utf-8
+from selenium import webdriver  
+from selenium.webdriver.common.keys import Keys  
+import selenium.webdriver.support.ui as ui  
+from selenium.webdriver.common.action_chains import ActionChains  
+from bs4 import BeautifulSoup as bs
+import time      
+import re      
+import os
+import csv
+
+#打开Firefox浏览器
+driver = webdriver.PhantomJS()
+driver1 = webdriver.PhantomJS()
+i = 0
+while i<1:
+    num = i*20
+    url ="https://movie.douban.com/subject/1292052/reviews?start="+str(num)
+    print(url)
+    driver.get(url)
+    #用户姓名 超链接
+    elem1 = driver.find_elements_by_xpath("//div[@class='main-bd']/h2/a")
+    for n in elem1:
+        print(n.text) 
+        link = n.get_attribute("href")
+        print(link,type(link))
+        driver1.get(str(link))
+        soup = bs(driver1.page_source,'lxml')
+        con = soup.find_all("div",{"class":"review-content clearfix"})
+        print(con)
+        
+        
+    i = i + 1
